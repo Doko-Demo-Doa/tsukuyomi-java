fcf65dd810443149e535b525f60d35d69e3dfe67
Quan Pham <doraemonfanclub@gmail.com>
2019-01-27 14:12:42 +0700

    Added new dependencies, added new constants

---
+++ src/main/java/com/clipsub/tsukuyomi/constant/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.clipsub.tsukuyomi.constant;
+
+public class CommonConstants {
+    public static final String SYSTEM_ACCOUNT = "system";
+    public static final String ANONYMOUS_USER = "anonymoususer";
+
+    private CommonConstants() {}
+}
