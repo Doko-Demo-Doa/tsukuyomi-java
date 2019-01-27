908c9230e1bbda47273dcb1470b39716206a88f7
Quan Pham <doraemonfanclub@gmail.com>
2019-01-27 17:46:39 +0700

    Added manga entity

---
+++ src/main/java/com/clipsub/tsukuyomi/entities/Manga.java
@@ -0,0 +1,17 @@
+package com.clipsub.tsukuyomi.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Manga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
