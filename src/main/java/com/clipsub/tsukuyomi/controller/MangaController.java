a89be01b2aa1e59c9844988f051372ff8486946a
Quan Pham <doraemonfanclub@gmail.com>
2019-04-05 03:16:39 +0700

    Make createManga accept formurlencoded

--- src/main/java/com/clipsub/tsukuyomi/controller/MangaController.java
+++ src/main/java/com/clipsub/tsukuyomi/controller/MangaController.java
@@ -3,9 +3,11 @@ package com.clipsub.tsukuyomi.controller;
 import com.clipsub.tsukuyomi.entity.Manga;
 import com.clipsub.tsukuyomi.repository.MangaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/manga", produces = "application/json", consumes = "multipart/form-data")
@@ -27,5 +29,10 @@ public class MangaController {
     @PostMapping
     public Manga createManga(@RequestBody Manga manga) {
         return mangaRepository.save(manga);
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public Manga createManga(@RequestParam String name) {
+        return mangaRepository.save(new Manga(name));
     }
 }
