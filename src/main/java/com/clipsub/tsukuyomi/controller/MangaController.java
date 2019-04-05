f17044b96e15bc4375480acc3d76cde3d01f2f6b
Quan Pham <doraemonfanclub@gmail.com>
2019-04-05 19:15:21 +0700

    Extend logging levels

--- src/main/java/com/clipsub/tsukuyomi/controller/MangaController.java
+++ src/main/java/com/clipsub/tsukuyomi/controller/MangaController.java
@@ -2,31 +2,21 @@ package com.clipsub.tsukuyomi.controller;
 
 import com.clipsub.tsukuyomi.entity.Manga;
 import com.clipsub.tsukuyomi.repository.MangaRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/manga", produces = "application/json", consumes = "multipart/form-data")
+@RequestMapping(path = "/manga", produces = "application/json")
 public class MangaController {
-
-    Logger logger = LoggerFactory.getLogger(MangaController.class);
 
     @Autowired
     private MangaRepository mangaRepository;
 
     @GetMapping("/")
     public List<Manga> getAllManga() {
-        RestTemplate rest = new RestTemplate();
-        String foo = "http://dummy.restapiexample.com/api/v1/employees";
-        String result = rest.getForObject(foo, String.class);
-
-        logger.info(result);
         return (List<Manga>) mangaRepository.findAll();
     }
 
--- src/main/java/com/clipsub/tsukuyomi/entity/Chapter.java
+++ src/main/java/com/clipsub/tsukuyomi/entity/Chapter.java
@@ -3,8 +3,6 @@ package com.clipsub.tsukuyomi.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "chapter")
 public class Chapter {
     public Chapter(){}
 }
