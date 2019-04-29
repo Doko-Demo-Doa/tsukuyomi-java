cfe791c7d648901a73b69fb58a032d63889ebacc
Quan Pham <doraemonfanclub@gmail.com>
2019-04-29 11:13:51 +0700

    Code cleanup

---
+++ src/main/java/com/clipsub/tsukuyomi/service/MangaClient.java
@@ -0,0 +1,5 @@
+package com.clipsub.tsukuyomi.service;
+
+public interface MangaClient {
+    void getManga();
+}
--- src/main/java/com/clipsub/tsukuyomi/service/MangafoxClient.java
+++ src/main/java/com/clipsub/tsukuyomi/service/MangafoxClient.java
@@ -1,9 +1,7 @@
 package com.clipsub.tsukuyomi.service;
 
 import com.clipsub.tsukuyomi.entity.Manga;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.client.Traverson;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
