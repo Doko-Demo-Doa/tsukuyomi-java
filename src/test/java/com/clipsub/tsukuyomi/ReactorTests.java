74651fac04bfa379a3869be0b306a499759957cb
Quan Pham <doraemonfanclub@gmail.com>
2019-05-20 14:55:30 +0700

    Added interval test

--- src/test/java/com/clipsub/tsukuyomi/ReactorTests.java
+++ src/test/java/com/clipsub/tsukuyomi/ReactorTests.java
@@ -4,17 +4,31 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 public class ReactorTests {
 
     @Test
     public void createFluxFromArray() {
-        String[] fruits = new String[] {"Apple", "Orange", "Grape"};
+        String[] fruits = new String[]{"Apple", "Orange", "Grape"};
         Flux<String> fruitFlux = Flux.fromArray(fruits);
 
         StepVerifier.create(fruitFlux)
                 .expectNext("Apple")
                 .expectNext("Orange")
                 .expectNext("Grape")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxFromInterval() {
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1)).take(5);
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
                 .verifyComplete();
     }
 }
